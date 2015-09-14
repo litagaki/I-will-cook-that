e3010b97ea851eaf52308651e1f7dd017e4ec17e
sigma29 <litagaki@gmail.com>
2015-09-13 20:03:35 -0400

    Add helper methods for user recognition, log-in & log-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :is_logged_in?
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    return unless session[:session_token]
+    user = User.find_by(session_token: session[:session_token])
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nilå
+  end
+
+  def is_logged_in?
+    !!current_user
+  end
+
 end
