ce2bca7572d5f4d45ccc4e100552b44698e678aa
sigma29 <litagaki@gmail.com>
2015-09-16 11:30:53 -0400

    Do initial styling of recipe display

--- app/assets/javascripts/models/recipe.js
+++ app/assets/javascripts/models/recipe.js
@@ -2,7 +2,7 @@ IWillCookThat.Models.Recipe = Backbone.Model.extend({
   urlRoot: '/api/recipes',
 
   toJSON: function(){
-    var json = Backbone.Models.prototype.toJSON.call(this);
+    var json = Backbone.Model.prototype.toJSON.call(this);
 
     return { recipe: json };
   }
